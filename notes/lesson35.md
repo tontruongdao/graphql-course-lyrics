e5b981a1fc406a944c1685ee2521fc4a2ee03eee
Truong Dao <ton.truong.dao@outlook.com>
2021-04-24 17:28:11 -0400

    Initialized Project

---
+++ notes/lesson35.md
@@ -0,0 +1,5 @@
+- Run following code to enable mongoDB
+
+```js
+npm install --save mongoose@5.5.6
+```
---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const models = require('./models');
+const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+
+const schema = require('./schema/schema');
+
+const app = express();
+
+// Replace with your mongoLab URI
+const MONGO_URI = process.env.MONGO;
+if (!MONGO_URI) {
+  throw new Error('You must provide a MongoLab URI');
+}
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URI);
+mongoose.connection
+    .once('open', () => console.log('Connected to MongoLab instance.'))
+    .on('error', error => console.log('Error connecting to MongoLab:', error));
+
+app.use(bodyParser.json());
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config.js');
+app.use(webpackMiddleware(webpack(webpackConfig)));
+
+module.exports = app;

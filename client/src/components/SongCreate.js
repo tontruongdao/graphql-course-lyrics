372fc271097c791a03366f708780093e8e9c119f
Truong Dao <ton.truong.dao@outlook.com>
2021-04-26 11:31:33 -0400

    Used useMutation to create song

--- client/src/components/SongCreate.js
+++ client/src/components/SongCreate.js
@@ -1,21 +1,48 @@
 import React, { useState } from 'react'
+import { gql, useMutation } from '@apollo/client'
+
+
+
 
 const SongCreate = () => {
-
-  const [title, setTitle] = useState("")
-  console.log(`Current title is ${title}`)
-
+  
+  const [songtitle, setSongtitle] = useState("")
+  let title;
+  // console.log(`Current title is ${songtitle}`)
+  
+  const [songCreate, { data }] = useMutation(CREATE_SONG)
+  let input;
+  
+  const onSubmit = (event) => {
+    event.preventDefault();
+    songCreate({ variables: { title: title.value } });
+    title.value = '';
+  }
+  
   return (
     <div>
       <h3>Create A New Song</h3>
-      <form action="">
+      <form onSubmit={onSubmit}>
         <label>Song Title:</label>
         <input 
-          onChange={e => setTitle(e.target.value)}
-          value={title}/>
+          onChange={e => setSongtitle(e.target.value)}
+          value={songtitle}
+          ref={value => {
+            title = value
+          }}/>
+        <button type="submit">Add Song</button>
       </form>
     </div>
   )
 }
+
+const CREATE_SONG = gql`
+mutation AddSong($title: String){
+  addSong(title: $title) {
+    id
+    title
+  }
+}
+`
 
 export default SongCreate;
